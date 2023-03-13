c1db220c9e6d532bb0cefceefa9a76d76d1c99c8
Jakub Falkiewicz <kufal222@gmail.com>
2023-03-14 00:37:53 +0100

    sign-up buttons

---
+++ src/assets/index.ts
@@ -0,0 +1,6 @@
+import FacebookIcon from "./images/Facebook.png"
+import GoogleIcon from "./images/Google.png"
+
+export {
+    FacebookIcon, GoogleIcon
+}
--- src/modules/HomeModule/components/Register.tsx
+++ src/modules/HomeModule/components/Register.tsx
@@ -1,11 +1,51 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./Register.scss"
+import { createUserWithEmailAndPassword } from "firebase/auth";
+import { auth } from '@/firebase';
+import { FacebookIcon, GoogleIcon } from '@/assets';
 
 function Register() {
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+
+    function authenticate() {
+        console.log("siema")
+        createUserWithEmailAndPassword(auth, email, password)
+            .then((userCredential) => {
+                const user = userCredential.user;
+                console.log(user)
+            })
+            .catch((error) => {
+                const errorCode = error.code;
+                const errorMessage = error.message;
+                console.log(errorCode, errorMessage)
+            });
+    }
+
     return (
         <div className='register_component'>
             <div className='register_component_content'>
-                Spotify
+                <div className='register_component_welcome'>
+                    <div className='register_component_logo'>Spotify</div>
+                    <div className='welcome_text'>Sign up for free to start listening.</div>
+                    <div className='register_component_welcome_buttons'>
+                        <button className='register_component_facebook_button'>
+                            <div className='img'>
+                                <img className='facebook' src={FacebookIcon}></img>
+                            </div>
+                            <div >Sign up with Facebook</div>
+                        </button>
+                        <button className='register_component_google_button'>
+                            <img className='google' src={GoogleIcon}></img>
+                            <div>Sign up with Google</div>
+                        </button>
+                    </div>
+                </div>
+                <div>Email</div>
+                <input onChange={(e) => setEmail(e.target.value)} />
+                <div>Password</div>
+                <input onChange={(e) => setPassword(e.target.value)} />
+                <button onClick={() => authenticate()}>Register</button>
             </div>
         </div>
     )
